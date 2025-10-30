44d8725ed8bd92751e5fef604a65d69d2d9e4d23
navibluer <navibluer@gmail.com>
2025-10-30 17:45:28 +0800

    fix pinned and date value in article data

--- apis/apiConfig.js
+++ apis/apiConfig.js
@@ -1,7 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL;
-export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-export const USER = import.meta.env.VITE_USER;
 export const HEADERS = {
-  "Access-Control-Allow-Origin": "*",
-  "Content-Type": "application/json",
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json',
 };
